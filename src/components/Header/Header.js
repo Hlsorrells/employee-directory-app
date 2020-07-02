e4d6173ef3dd92068ae684d4f37a24f2bfaeabc7
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-01 23:20:24 -0400

    created a basic header layout to render

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1>ACME IT Consulting, Inc.</h1>
+      <h3>Employee List</h3>
+    </div>
+  );
+}
+
+export default Jumbotron;
