4628b876399dea8639aa257793a98e147905fcc4
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-01 23:19:45 -0400

    created a basic employee card layout to render

---
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3 className="card-text">Employee Name:<span></span></h3>
+        <p className="card-text">Address:<span></span></p>
+        <p className="card-text">Phone:<span></span></p>
+        <p className="card-text">Email:<span></span></p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
