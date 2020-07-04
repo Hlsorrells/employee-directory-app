cb2b97c8f0e6997ebe243a6d5fc2c7fac0f34a78
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-03 21:06:00 -0400

    added sort feature to id number column

--- src/components/EmployeeList/EmployeeList.js
+++ src/components/EmployeeList/EmployeeList.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
 import EmployeeListItem from '../EmployeeListItem/EmployeeListItem';
 import Search from '../Search/Search';
 import employees from '../../lib/employees.json';
 
 
 const EmployeeList = () => {
-    const [employeeList, setEmployeeList] = useState(employees)
+    const [employeeList, setEmployeeList] = useState(employees);
+
+    const handleClick = () => {
+        const sorted = () => [...employeeList].sort((a, b) => (a.id.value) > (b.id.value) ? 1 : -1);
+        setEmployeeList(sorted);
+    };
 
     return (
         <div>
@@ -13,33 +19,32 @@ const EmployeeList = () => {
                 employees={employees}
                 filterList={setEmployeeList}
             />
-            <div className="table-responsive">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Photo</th>
-                            <th scope="col">ID#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(employeeList.length > 0) ? employeeList.map((employee, index) => (
-                            <EmployeeListItem
-                                key={index}
-                                id={employee.id}
-                                name={employee.name}
-                                image={employee.picture.thumbnail}
-                                email={employee.email}
-                                phone={employee.phone}
-                            />
-                        )) : <p>No employees</p>}
-                    </tbody>
-                </table>
-            </div>
+            <Table responsive striped bordered hover>
+                <thead>
+                    <tr>
+                        <th scope="col">Photo</th>
+                        <th scope="col">
+                            <Button variant="link" id="id" onClick={(e) => { handleClick(e.target.id) }}>ID#</Button>
+                        </th>
+                        <th scope="col">Employee Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {(employeeList.length > 0) ? employeeList.map((employee, index) => (
+                        <EmployeeListItem
+                            key={index}
+                            id={employee.id}
+                            name={employee.name}
+                            image={employee.picture.thumbnail}
+                            email={employee.email}
+                            phone={employee.phone}
+                        />
+                    )) : <p>No employees</p>}
+                </tbody>
+            </Table>
         </div>
-
     );
 }
 export default EmployeeList;
